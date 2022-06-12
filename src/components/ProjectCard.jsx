0d8a3d42a0aa91cc9c946cc8bd7bf72e531bded7
John D <jddhalogamer@gmail.com>
2022-06-12 18:21:27 -0500

    added preview images to project cards, and proper styling to buttons.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,16 +2,17 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 
-const ProjectCard = ( {projectTitle, projectUrl} ) => {
+
+const ProjectCard = ( {projectTitle, projectUrl, previewImage, previewBlurb} ) => {
   return (
 	<Card className="project-card">
-  		<Card.Img variant="top" src="holder.js/100px180" />
+  		<Card.Img 
+		  variant="top" 
+		  src={previewImage} 
+		  style={{ width: '150px', height: '100px', border: '3px solid blue' }} />
   		<Card.Body>
     		<Card.Title>{projectTitle}</Card.Title>
-    		<Card.Text>
-      		Some quick example text to build on the card title and make up the bulk of
-      		the card's content.
-    		</Card.Text>
+    		<Card.Text>{previewBlurb}</Card.Text>
     	<Button className="btn" variant="primary" href={projectUrl} >View Project</Button>
   		</Card.Body>
 	</Card>
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,4 +1,7 @@
 import ProjectCard from './ProjectCard'
+import BudgetAppPreview from '../images/BudgetTrackerPreview.jpeg'
+import PokeDexPreview from '../images/PokedexPreview.jpeg'
+import CalculatorPreview from '../images/CalculatorPreview.jpeg'
 
 const ProjectSection = () => {
   return (
@@ -8,14 +11,20 @@ const ProjectSection = () => {
 		<ProjectCard 
 		projectTitle={'Budget Tracker App'}
 		projectUrl={'https://jdixon1995.github.io/React-Budget-App/'}
+		previewImage={BudgetAppPreview}
+		previewBlurb={'A sleek budget tracker app built w/ React, Hooks, and styled w/ Bootstrap.'}
 		/>
 		<ProjectCard 
 		projectTitle={'PokeDex App'}
 		projectUrl={'https://jdixon1995.github.io/PokeAPI-w-React/'}
+		previewImage={PokeDexPreview}
+		previewBlurb={'This displays my first app built to consume an external API. This app features searching, and pagination of results.'}
 		/>
 		<ProjectCard 
 		projectTitle={'JS Calculator'}
 		projectUrl={'https://jdixon1995.github.io/My-Calculator-JS/'}
+		previewImage={CalculatorPreview}
+		previewBlurb={'A simple app I built to get my toes wet with Javascript. It works just like one on your desk.'}
 		/>
 		</div>
 	</div>
