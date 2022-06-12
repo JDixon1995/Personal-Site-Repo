f0b9eb088ffb9e1c46561679a88132d574c337dd
John D <jddhalogamer@gmail.com>
2022-06-11 21:37:57 -0500

    added bio.

--- src/components/BlockSection.jsx
+++ src/components/BlockSection.jsx
@@ -6,7 +6,23 @@ const BlockSection = () => {
 		<div className="blurb-block">
 			<h4>The Nitty Gritty</h4>
 			<article>
-				I'm a Developer with a preference for JavaScript. 
+				I'm a full-stack developer with an intermediate proficiency 
+				in HTML, CSS, and JS development. I am very comfortable working
+				with the MERN stack of technologies. My version control software
+				of choice is Git, with GitHub hosting my repositories.
+			</article>
+			<article>
+				Furthermore, I possess a fundemental knowledge of industry
+				best practices in software design concepts, working with APIs,
+				and object-oriented programming.
+			</article>
+			<article>
+				My future aspirations are to hone my skills in the area of full-stack
+				development until I reach my desired level of expertise.
+			</article>
+			<article>
+				My passions lie in being a lifelong learner, martial arts
+				practitioner, and an outdoorsman.
 			</article>
 		</div>
 	</div>
