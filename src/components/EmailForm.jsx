762a4539157610a064d6070caebfd52674e4c0c7
John D <jddhalogamer@gmail.com>
2022-06-12 18:52:02 -0500

    added state hook to contact form for email composition.

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,24 +1,81 @@
+import { useState } from 'react'
+import { send } from 'emailjs-com'
+
 
 const EmailForm = () => {
+
+	const [toSend, setToSend] = useState({
+		name: '',
+		subject: '',
+		email: '',
+		text: '',
+	})
+
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		send(
+			'SERVICE ID',
+			'TEMPLATE ID',
+			toSend,
+			'User ID',
+		)
+		.then((response) => {
+			console.log('SUCCESS!', response.status, response.text)
+		})
+		.catch((error) => {
+			console.log('FAILED...', error)
+		})
+	}
+
+	const handleChange = (e) => {
+		e.preventDefault()
+		setToSend({...toSend, [e.target.name]: e.target.value})
+	}	
+
   return (
-	<div class="email-container">
+	<div className="email-container">
 			<a href=""></a>
 			<h4>Get In Touch</h4>
-			<form className="form-control">
+			<form className="form-control" onSubmit={onSubmit}>
 				<a name="contact"></a>
 				<label>Name</label>
-				<input class="input-field" type="text" name="name"></input>
+				<input 
+				type="text" 
+				name="name"
+				placeholder="Name here..."
+				value={toSend.name}
+				onChange={handleChange}
+				></input>
 
 				<label>Subject</label>
-				<input class="input-field" type="text" name="subject"></input>
+				<input 
+				type="text" 
+				name="subject"
+				placeholder="Subject here..."
+				value={toSend.subject}
+				onChange={handleChange}
+				></input>
 
 				<label>Email</label>
-				<input class="input-field" type="text" name="email"></input>
+				<input 
+				type="text" 
+				name="email"
+				placeholder="Email Here..."
+				value={toSend.email}
+				onChange={handleChange}
+				></input>
 
 				<label>Message</label>
-				<textarea class="input-field" name="message"></textarea>
+				<textarea
+				type="text" 
+				name="text"
+				placeholder="Message here..."
+				value={toSend.text}
+				onChange={handleChange}
+				></textarea>
 			</form>
-			<button>Send</button>
+			<button type="submit">Send</button>
 		</div>
   )
 }
